d8f298e2e72bfeae46a50bdf82fcfc489a01c28d
Zanzibar <zanzisworld42@gmail.com>
2025-07-30 09:37:17 -0700

    Final

--- rust-bucket/guessing_game/src/main.rs
+++ rust-bucket/guessing_game/src/main.rs
@@ -7,18 +7,27 @@ fn main() {
     println!("What's my number between 1 and 100?");
     
     let secret_number = rand::rng().random_range(1..=100);
-    println!("The secret number is: {secret_number}");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number.");
-    println!("You guessed: {guess}");
+    loop {
+        println!("What's your guess...?");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small..."),
-        Ordering::Greater => println!("Too big!!!"),
-        Ordering::Equal => println!("You win!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small..."),
+            Ordering::Greater => println!("Too big!!!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
