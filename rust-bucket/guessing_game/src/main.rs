1ecad65370c93f2d387a0a9d33befa79f589af9a
Zanzibar <zanzisworld42@gmail.com>
2025-07-30 09:19:10 -0700

    Save

--- rust-bucket/guessing_game/src/main.rs
+++ rust-bucket/guessing_game/src/main.rs
@@ -1,14 +1,24 @@
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
 fn main() {
     println!("Let's play a guessing game!");
+    println!("What's my number between 1 and 100?");
+    
     let secret_number = rand::rng().random_range(1..=100);
     println!("The secret number is: {secret_number}");
-    println!("What's my number between 1 and 100?");
+
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Please type a number.");
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small..."),
+        Ordering::Greater => println!("Too big!!!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
