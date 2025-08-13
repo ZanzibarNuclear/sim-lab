aa8f0fcda992bb0c150629969de762dfedf05029
Zanzibar <zanzisworld42@gmail.com>
2025-08-13 15:27:41 -0700

    Save

--- rust-bucket/scraper/src/main.rs
+++ rust-bucket/scraper/src/main.rs
@@ -1,19 +1,47 @@
 use std::time::Duration;
+use std::pin::{Pin, pin};
 
 fn main() {
     trpl::run(async {
-        let handle = trpl::spawn_task(async {
-            for i in 1..10 {
-                println!("hi number {i} from the first task!");
-                trpl::sleep(Duration::from_millis(500)).await;
+
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = pin!(async move {
+            let vals = vec![
+                String::from("beep"),
+                String::from("beep"),
+                String::from("beep"),
+                String::from("beep"),
+            ];
+    
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1000)).await;
             }
         });
         
-        for i in 1..5 {
-            println!("hi number {i} from second task!");
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+        let rx_fut = pin!(async {
+            while let Some(value) = rx.recv().await {
+                println!("Got: {value}");
+            }
+        });
+        
+        let tx_fut = pin!(async move {
+            let vals = vec![
+                String::from("bop"),
+                String::from("bop"),
+                String::from("bop"),
+            ];
+    
+            for val in vals {
+                trpl::sleep(Duration::from_millis(750)).await;
+                tx.send(val).unwrap();
+            }
+        });
 
-        handle.await.unwrap();
+        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> = vec![tx1_fut, rx_fut, tx_fut];
+
+        trpl::join_all(futures).await;
     })
 }
