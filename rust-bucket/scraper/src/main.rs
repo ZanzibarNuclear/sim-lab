2731290154b4df4cb11cba74f7cefe639e233083
Zanzibar <zanzisworld42@gmail.com>
2025-08-13 16:09:42 -0700

    Practice yield

--- rust-bucket/scraper/src/main.rs
+++ rust-bucket/scraper/src/main.rs
@@ -1,47 +1,38 @@
 use std::time::Duration;
-use std::pin::{Pin, pin};
+use std::thread;
 
 fn main() {
     trpl::run(async {
 
-        let (tx, mut rx) = trpl::channel();
-
-        let tx1 = tx.clone();
-        let tx1_fut = pin!(async move {
-            let vals = vec![
-                String::from("beep"),
-                String::from("beep"),
-                String::from("beep"),
-                String::from("beep"),
-            ];
-    
-            for val in vals {
-                tx1.send(val).unwrap();
-                trpl::sleep(Duration::from_millis(1000)).await;
-            }
-        });
+        let a = async {
+            println!("'a' started");
+            slow("a", 30);
+            trpl::yield_now().await;
+            slow("a", 10);
+            trpl::yield_now().await;
+            slow("a", 20);
+            trpl::yield_now().await;
+            println!("'a' finished");
+        };
+        let b = async {
+            println!("'b' started");
+            slow("b", 75);
+            trpl::yield_now().await;
+            slow("b", 10);
+            trpl::yield_now().await;
+            slow("b", 15);
+            trpl::yield_now().await;
+            slow("b", 350);
+            trpl::yield_now().await;
+            println!("'b' finished");
+        };
         
-        let rx_fut = pin!(async {
-            while let Some(value) = rx.recv().await {
-                println!("Got: {value}");
-            }
-        });
-        
-        let tx_fut = pin!(async move {
-            let vals = vec![
-                String::from("bop"),
-                String::from("bop"),
-                String::from("bop"),
-            ];
-    
-            for val in vals {
-                trpl::sleep(Duration::from_millis(750)).await;
-                tx.send(val).unwrap();
-            }
-        });
+        trpl::race(a, b).await;
 
-        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> = vec![tx1_fut, rx_fut, tx_fut];
-
-        trpl::join_all(futures).await;
     })
+}
+
+fn slow(name: &str, ms: u64) {
+    thread::sleep(Duration::from_millis(ms));
+    println!("'{name}' ran for {ms}ms");
 }
