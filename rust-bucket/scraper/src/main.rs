7bf3b247db3c7f5471ccfbdb5cde62c996eac76e
Zanzibar <zanzisworld42@gmail.com>
2025-08-14 08:24:56 -0700

    Timeout

--- rust-bucket/scraper/src/main.rs
+++ rust-bucket/scraper/src/main.rs
@@ -1,38 +1,29 @@
 use std::time::Duration;
-use std::thread;
+use trpl::Either;
 
 fn main() {
     trpl::run(async {
 
-        let a = async {
-            println!("'a' started");
-            slow("a", 30);
-            trpl::yield_now().await;
-            slow("a", 10);
-            trpl::yield_now().await;
-            slow("a", 20);
-            trpl::yield_now().await;
-            println!("'a' finished");
+        let slow = async {
+            trpl::sleep(Duration::from_secs(5)).await;
+            "That's all folks!"
         };
-        let b = async {
-            println!("'b' started");
-            slow("b", 75);
-            trpl::yield_now().await;
-            slow("b", 10);
-            trpl::yield_now().await;
-            slow("b", 15);
-            trpl::yield_now().await;
-            slow("b", 350);
-            trpl::yield_now().await;
-            println!("'b' finished");
-        };
-        
-        trpl::race(a, b).await;
 
-    })
+        match timeout(slow, Duration::from_secs(2)).await {
+            Ok(message) => println!("Succeeded with '{message}'"),
+            Err(duration) => {
+                println!("Failed after {} seconds", duration.as_secs())
+            }
+        }
+    });
 }
 
-fn slow(name: &str, ms: u64) {
-    thread::sleep(Duration::from_millis(ms));
-    println!("'{name}' ran for {ms}ms");
+async fn timeout<F: Future>(
+    future_to_try: F,
+    max_time: Duration,
+) -> Result<F::Output, Duration> {
+    match trpl::race(future_to_try, trpl::sleep(max_time)).await {
+        Either::Left(output) => Ok(output),
+        Either::Right(_) => Err(max_time),
+    }
 }
