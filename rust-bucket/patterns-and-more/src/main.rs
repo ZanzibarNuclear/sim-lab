ca5acf2eb1cfde48a951e9bd51ff723f3029d5e1
Zanzibar <zanzisworld42@gmail.com>
2025-08-19 16:45:23 -0700

    Save unsafe samples

--- rust-bucket/patterns-and-more/src/main.rs
+++ rust-bucket/patterns-and-more/src/main.rs
@@ -1,43 +1,61 @@
+use std::slice;
+
+unsafe trait Foo {}
+unsafe impl Foo for i32 {}
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
 fn main() {
-    let favorite_color: Option<&str> = None;
-    let is_tuesday = false;
-    let age: Result<u8, _> = "34".parse();
-
-    if let Some(color) = favorite_color {
-        println!("Using your favorite color, {color}, as the background");
-    } else if is_tuesday {
-        println!("Tuesday is green day");
-    } else if let Ok(age) = age {
-        if age > 30 {
-            println!("Using purple as the background color");
-        } else {
-            println!("Using orange as the background color");
-        }
-    } else {
-        println!("Using blue as the background color");
+    unsafe {
+        add_to_count(3);
+        println!("COUNTER: {}", *(&raw const COUNTER));
     }
 
-    listen();
-    loopy();
+    // let mut v = vec![1, 2, 3, 4, 5, 6];
+    // let r = &mut v[..];
+    // let (a, b) = split_at_mut(r, 3);
+
+    // assert_eq!(a, &mut [1, 2, 3]);
+    // assert_eq!(b, &mut [4, 5, 6]);
+
+    // let mut num = 3;
+
+    // let r1 = &raw const num;
+    // let r2 = &raw mut num;
+
+    // num = num + 5;
+
+    // unsafe {
+    //     println!("num is {}", num);
+    //     println!("r1 is {}", *r1);
+    //     println!("r2 is {}", *r2);
+    // }
+
+    // unsafe fn dangerous() {
+    //     println!("What?");
+    // }
+
+    // unsafe {
+    //     dangerous();
+    // }
 }
 
-fn listen() {
-    let (tx, rx) = std::sync::mpsc::channel();
-    std::thread::spawn(move || {
-        for val in [1, 2, 3] {
-            tx.send(val).unwrap();
-        }
-    });
+static mut COUNTER: u32 = 0;
 
-    while let Ok(value) = rx.recv() {
-        println!("{value}");
-    }
-}
-
-fn loopy() {
-    let v = vec!['a', 'b', 'c'];
-
-    for (index, value) in v.iter().enumerate() {
-        println!("{value} is at index {index}")
+unsafe fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
     }
 }
